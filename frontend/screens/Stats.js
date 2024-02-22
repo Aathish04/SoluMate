ae55d97c69d0775e605ca91490fd16b03c6fb812
Parthiban <m.parthiban7845@gmail.com>
2024-02-22 13:32:36 +0530

    Added Stats.

---
+++ frontend/screens/Stats.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import NavigationBar from '../components/NavigationBar';
+import { useNavigation } from '@react-navigation/native';
+
+
+export default function Stats()
+{
+    
+
+
+
+
+    return (
+
+    );
+}
